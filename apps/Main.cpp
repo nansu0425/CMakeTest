b8309a9e2484a94c77f6135cd5755ae80fdbb6fe
NanSu <nansu0425@gmail.com>
2025-02-06 01:08:58 +0900

    Update CMake setup and add new library and executable
    Updated CMakeLists.txt with project settings, C++ standard, and IDE folder support. Replaced CompilerInterface with ILib. Added src and apps subdirectories. Created App executable target linked with Lib. Added Main.cpp and Functions.h with PrintHello function. Created Lib static library linked with ILib and set include directory. Updated Functions.cpp to define PrintHello.

---
+++ apps/Main.cpp
@@ -0,0 +1,6 @@
+#include <CMakeTest/Functions.h>
+
+int main(int argc, char* charv[]) {
+	PrintHello();
+	return 0;
+}
--- src/Functions.cpp
+++ src/Functions.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-int main(int argc, char* charv[]) {
+#include <CMakeTest/Functions.h>
+
+void PrintHello() {
 	std::cout << "Hello, World!" << std::endl;
-	return 0;
 }
